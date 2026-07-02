a0c65b3383151c2f8dfe68da252fb4ee173bde13
fahruldengo <142699753+fahruldengo@users.noreply.github.com>
2026-07-02 10:31:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,24 @@
    ========================================================= */
 window.LEDGERINE_CONFIG = {
   // Tempel URL Web App di sini, contoh:
-  // API_URL: "https://script.google.com/macros/s/AKfycbx4XbQvqoVqLzlxKUYQZ865GfODLVNIkRdjhqM2AaQYRGdiSo-0M1sjES2OUS1pGLZLSw/exec",
+  // API_URL: "https://script.google.com/macros/s/AKfycbx....../exec",
   API_URL: "",
 
   COMPANY: {
-    name:  "CV Vidya Amaliah",
-    addr:  "Manado, Sulawesi Utara",
-    phone: "0812-xxxx-xxxx",
-    email: "info@vidyaamaliah.co.id"
+    name:    "CV. VIDYA AMALIAH",
+    tagline: "Strategic Business Partner Telkomsel",
+    address: "Jl. Nani Wartabone Ruko Bonanza No.2 RT.002 RW. 004 Kel. Limba U1, Kota Selatan, Kota Gorontalo, GORONTALO, GORONTALO, 96111",
+    phone:   "0811435431",
+    email:   "cvvidyaamaliah@gmail.com",
+    npwp:    "934538901822000",
+    city:    "Gorontalo",
+    bank: {
+      name:    "PT Bank Mandiri Tbk.",
+      branch:  "Gorontalo",
+      account: "150 00 5551333 5",
+      holder:  "CV Vidya Amaliah"
+    },
+    signer:  "Farin Pohantalo",
+    title:   "General Manager"
   }
 };
